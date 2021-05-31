1eafb173ee919a567354263e3f5f6beaf4cbc60b
Jaco Swarts <jaco@productive.me>
2021-05-31 20:57:44 +0200

    Remove dist from repo

--- dist/src/reducers.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const reducers: {
-    sum: (hash: any) => any;
-    count: (hash: any) => any;
-    avg: (hash: any) => any;
-    yep: (hash: any) => any;
-    nope: (hash: any) => any;
-};
